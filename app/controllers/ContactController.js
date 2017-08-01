0a27e43e5a26911b7e37d7612389693f05981d21
Thang Pham <thangpham.it92@gmail.com>
2017-08-01 23:23:32 +0700

    init source code on heroku

--- app/controllers/ContactController.js
+++ app/controllers/ContactController.js
@@ -16,11 +16,29 @@ var transporter = nodemailer.createTransport({
 });
 
 var contactController = {
-    index: function (req, res) {
-        res.render('contact/lien-he');
+    index: function (req, res, next) {
+        res.render('contact/lien-he', {
+            title: 'Form validator',
+            success: req.session.success,
+            errors: req.body.errors
+        });
+        req.session.errors = null;
     },
 
-    sendEmailToEducation: function(req, res) {
+    sendEmailToEducation: function(req, res, next) {
+        // check validator
+        req.check('email', 'Email không hợp lệ!').isEmail();
+        req.check('full_name', 'Vui lòng điền họ tên!').notEmpty().isInt();
+
+        var errors = req.validationErrors();
+        if(errors){
+            req.session.errors = errors;
+            req.session.success - false;
+        } else {
+            req.session.success = true;
+        }
+        res.render('contact/lien-he')
+
         var full_name = req.body.full_name;
         var address = req.body.address;
         var email = req.body.email;
@@ -42,44 +60,44 @@ var contactController = {
         };
 
         // send mail with defined transport object
-        transporter.sendMail(mailOptions, function (error, info) {
-            if (error) {
-                res.render('modals/lien-he-modal', {
-                    flash: {
-                        type: 'error',
-                        title: 'LIÊN HỆ VỚI TRUNG TÂM THẤT BẠI',
-                        message: 'Bạn vui lòng liên lạc trực tiếp với trung tâm <b>TƯ VẤN DU HỌC ĐẠI HẢI</b>.' +
-                                 '<br>' +
-                                 'Liên hệ và ghi danh tại Văn phòng trụ sở chính: ' +
-                                 '<br>' +
-                                 '<b>18/64A, KDC Đại Hải, Ấp 7, xã Xuân Thới Thượng, huyện Hóc Môn, HCM.</b>' + 
-                                 '<br>' +
-                                 '<b>Số điện thoại</b>: 0862.50.55.22 hoặc 0914.787.008 (Thầy Nam).' + 
-                                 '<br>' +
-                                 '<b>Email</b>: tuvanduhocdaihai@gmail.com'
-                    }
-                });
-            } else {
-                // save contact into db
-                Contact.createContact({
-                    full_name: full_name,
-                    address: address,
-                    phone: phone,
-                    email: email,
-                    message: message
-                }, function(err){
-                });
-                res.render('modals/lien-he-modal', {
-                    flash: {
-                        type: 'success',
-                        title: 'LIÊN HỆ VỚI TRUNG TÂM THÀNH CÔNG',
-                        message: 'Cảm ơn bạn đã liên lạc với trung tâm <b>TƯ VẤN DU HỌC ĐẠI HẢI</b>.' + 
-                                '<br>' +
-                                'Chúng tôi sẽ liên lạc với bạn trong thời gian sớm nhất!'
-                    }
-                });
-            }
-        });
+    //     transporter.sendMail(mailOptions, function (error, info) {
+    //         if (error) {
+    //             res.render('modals/lien-he-modal', {
+    //                 flash: {
+    //                     type: 'error',
+    //                     title: 'LIÊN HỆ VỚI TRUNG TÂM THẤT BẠI',
+    //                     message: 'Bạn vui lòng liên lạc trực tiếp với trung tâm <b>TƯ VẤN DU HỌC ĐẠI HẢI</b>.' +
+    //                              '<br>' +
+    //                              'Liên hệ và ghi danh tại Văn phòng trụ sở chính: ' +
+    //                              '<br>' +
+    //                              '<b>18/64A, KDC Đại Hải, Ấp 7, xã Xuân Thới Thượng, huyện Hóc Môn, HCM.</b>' + 
+    //                              '<br>' +
+    //                              '<b>Số điện thoại</b>: 0862.50.55.22 hoặc 0914.787.008 (Thầy Nam).' + 
+    //                              '<br>' +
+    //                              '<b>Email</b>: tuvanduhocdaihai@gmail.com'
+    //                 }
+    //             });
+    //         } else {
+    //             // save contact into db
+    //             Contact.createContact({
+    //                 full_name: full_name,
+    //                 address: address,
+    //                 phone: phone,
+    //                 email: email,
+    //                 message: message
+    //             }, function(err){
+    //             });
+    //             res.render('modals/lien-he-modal', {
+    //                 flash: {
+    //                     type: 'success',
+    //                     title: 'LIÊN HỆ VỚI TRUNG TÂM THÀNH CÔNG',
+    //                     message: 'Cảm ơn bạn đã liên lạc với trung tâm <b>TƯ VẤN DU HỌC ĐẠI HẢI</b>.' + 
+    //                             '<br>' +
+    //                             'Chúng tôi sẽ liên lạc với bạn trong thời gian sớm nhất!'
+    //                 }
+    //             });
+    //         }
+    //     });
     }
 }
 
--- config/index.js
+++ config/index.js
@@ -2,6 +2,9 @@ var express         = require('express');
 var bodyParser      = require('body-parser');
 var expressLayouts  = require('express-ejs-layouts');
 var nodemailer      = require('nodemailer');
+var cookieParser    = require('cookie-parser');
+var expressValidator = require('express-validator');
+var expressSession  = require('express-session');
 
 module.exports = function (app) {
     // use ejs and express layouts
@@ -9,9 +12,11 @@ module.exports = function (app) {
     app.set('view engine', 'ejs');
     app.use(expressLayouts);
 
-    app.use(bodyParser.urlencoded({
-        extended: true
-    }));
     app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(expressValidator());
+    app.use(cookieParser());
+    app.use(expressSession({secret: 'max', saveUninitialized: false, resave: false}));
+
 }
 
